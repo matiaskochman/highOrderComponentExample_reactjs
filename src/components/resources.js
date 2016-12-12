5da82f5eeaa15f6d18f05df6fe08c8c6a282db63
Matias Kochman <matiaskochman@Matiass-MacBook-Pro.local>
2016-12-12 08:48:28 -0300

    first commit

---
+++ src/components/resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <ul>
+        <li>1 cup of sugar</li>
+        <li>1 cup of salt</li>
+        <li>1 cup of pepper</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+
+import require_Auth from './components/require_authentication';
+import App from './components/app';
+import reducers from './reducers';
+import Resources from './components/resources'
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="resources" component={require_Auth(Resources)}/>
+      </Route>
+    </Router>
+  </Provider>
+  , document.querySelector('.container'));
